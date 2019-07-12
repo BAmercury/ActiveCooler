fd722c4a46c0940fc7c8a3960f1e94c5f3c2bc3e
Brian Amin <bhautikamin97@gmail.com>
2019-07-12 12:10:45 -0400

    Added in Timer3 doing an interrupt routine to sample temperature sensor at a desired rate, need to test

--- ActiveCooler.ino
+++ ActiveCooler.ino
@@ -10,7 +10,6 @@
 #define PIN_TEMP_ONEWIRE 7
 #define PIN_MOTOR_OUTPUT 9
 
-
 // Temperature Ranges (fahrenheit)
 #define TEMP_MIN 70
 #define TEMP_MAX 105
@@ -19,6 +18,7 @@
 #define MOTOR_MAX 100 // (In terms of duty cycle) At TEMP_MAX this speed will engage
 
 #define PWM_TOP 639 // TOP Value for PWM
+#define INTERRUPT_TOP 199 // TOP Value for Data Sampling ISR
 
 // Configure onewire object
 OneWire oneWire(PIN_TEMP_ONEWIRE);
@@ -26,7 +26,9 @@ OneWire oneWire(PIN_TEMP_ONEWIRE);
 // Pass onewire object reference to Temperature library
 DallasTemperature sensors(&oneWire);
 
-int pwm = 0;
+volatile int pwm = 0;
+
+volatile int temperature = 0;
 
 // Convert given duty cycle to valid pwm value
 long DUTY2PWM(int duty)
@@ -43,49 +45,25 @@ long DUTY2PWM(int duty)
 
 }
 
-void setup(void)
+ISR(TIMER3_CAPT_vect)
 {
-    Serial.begin(115200);
-
-    // Start the temperature library
-    sensors.begin();
-    pinMode(PIN_MOTOR_OUTPUT, OUTPUT);
-
-    // Using Timer 1 for PWM (OC1A, PB5, Pin 9 on Sparkfun Board)
-    // Set Timer 1 for 25kHz frequency
-    // 25kHz = (16 Mhz / PRESCALER) / (TOP + 1)
-    // Prescaler = 1, TOP = 639
-    TCCR1A = (_BV(COM1A1) |_BV(WGM11) ); // Noninverting PWM signal
-    TCCR1B = (_BV(WGM13) | _BV(WGM12) | _BV(CS10) ); // Prescaler 1
-    TCCR1C = 0;
-    ICR1 = PWM_TOP; // TOP value
-    DDRB |= _BV(DDB5); // Enable output
-
-
-
-}
-
-void loop(void)
-{
-
-
     /*
         sensors.requestTemperatures() issues a global command
         to all connected sensors on the bus
      */ 
     sensors.requestTemperatures();
-    float temp = sensors.getTempFByIndex(0);
+    temperature = sensors.getTempFByIndex(0);
 
     // Refer to DallasTemperature.h for error codes
     // If below negative -196.6 yield an error
     // If the sensor wire is disconnected or if sensor is removed from ground
-    if (temp != DEVICE_DISCONNECTED_F)
+    if (temperature != DEVICE_DISCONNECTED_F)
     {
         Serial.print("Temperatuare (F): ");
-        Serial.print(temp);
+        Serial.print(temperature);
         Serial.print(", ");
         // Map the temperature to fan pwm value
-        pwm = map(temp, TEMP_MIN, TEMP_MAX, DUTY2PWM(MOTOR_MIN), DUTY2PWM(MOTOR_MAX));
+        pwm = map(temperature, TEMP_MIN, TEMP_MAX, DUTY2PWM(MOTOR_MIN), DUTY2PWM(MOTOR_MAX));
         Serial.print("PWM: ");
         // Apply limits (0-639)
         if (pwm > PWM_TOP)
@@ -100,14 +78,41 @@ void loop(void)
         pwm = 0;
     }
 
+}
 
+void setup(void)
+{
+    Serial.begin(115200);
+    // Start the temperature library
+    sensors.begin();
+    pinMode(PIN_MOTOR_OUTPUT, OUTPUT);
+    // Using Timer 1 for PWM (OC1A, PB5, Pin 9 on Sparkfun Board)
+    // Set Timer 1 for 25kHz frequency
+    // 25kHz = (16 Mhz / PRESCALER) / (TOP + 1)
+    // Prescaler = 1, TOP = 639
+    TCCR1A = (_BV(COM1A1) |_BV(WGM11) ); // Noninverting PWM signal
+    TCCR1B = (_BV(WGM13) | _BV(WGM12) | _BV(CS10) ); // Prescaler 1
+    TCCR1C = 0;
+    ICR1 = PWM_TOP; // TOP value
+    DDRB |= _BV(DDB5); // Enable output
+
+    // Timer 3 Setup for sampling temperature sensor
+    // Sampling Rate: 40Khz
+    TCCR3A = 0;
+    TCCR3B = (_BV(WGM33) | _BV(WGM32) | _BV(CS30)); // Prescaler 1, Mode 12 WGM
+    TCCR3C = 0;
+    ICR3 = INTERRUPT_TOP; // TOP value
+    sei(); // Global Interrupt Enable
+    TIFR3 = (_BV(ICF3)); // Enable Input Capture Flag
+    TIMSK3 = (_BV(ICIE3)); // Enable Input Capture Interrupt
+
+}
+
+void loop(void)
+{
     // Pin 9 on Sparkfun Pro Micro is connected to Timer 1
-    OCR1A = pwm; // Set PWM
-
-
-
-
-
-
-
+    cli(); // Disable Interrupts
+    int output = pwm; // Copy PWM value
+    sei(); // Enable Interrupts
+    OCR1A = output; // Set PWM
 }
